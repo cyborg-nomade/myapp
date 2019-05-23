b06526a1113c29a3808f15cecbf9f09f552c609e
cybnom <uriel.fiori@gmail.com>
2019-05-22 22:00:28 -0300

    events practice

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,19 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'myapp';
+  text:string;
+
+  constructor(){
+    this.text = 'Hello';
+  }
+
+  clicked (event) {
+    console.log(event);
+    this.text='Button clicked'
+  }
+
+  showText (event) {
+    //console.log(event.target.value);
+    console.log(this.text);
+  }
 }
